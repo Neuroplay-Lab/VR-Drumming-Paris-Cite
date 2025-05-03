94b8e49eb59062a545fa6cd9a172b2373e629b33
Jack Smith <jackesmith00@gmail.com>
2025-05-03 18:16:35 +0100

    Reduced controller vibration time to .25 secs

--- Assets/_Project/Scripts/Field/DrumStick.cs
+++ Assets/_Project/Scripts/Field/DrumStick.cs
@@ -50,11 +50,11 @@ namespace DrumRhythmGame.Field
         {
             if (hand == Hand.Left)
             {
-                hapticAction.Execute(0, 1, 150, 75, SteamVR_Input_Sources.LeftHand);
+                hapticAction.Execute(0, 0.25f, 150, 75, SteamVR_Input_Sources.LeftHand);
             }
             else
             {
-                hapticAction.Execute(0, 1, 150, 75, SteamVR_Input_Sources.RightHand);
+                hapticAction.Execute(0, 0.25f, 150, 75, SteamVR_Input_Sources.RightHand);
             }
 
             // below uses xr method which may not be working
