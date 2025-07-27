5da4f49af41adf12a9250734d861de057f003c83
Jack Smith <jackesmith00@gmail.com>
2025-07-27 21:38:10 +0100

    Added functionality for Music Sequences in playlists, along with special condition to add a break. Next need to implement recall stage, that starts when a participant first hits the drum.

--- Assets/_Project/Scripts/Systems/PlaylistController.cs
+++ Assets/_Project/Scripts/Systems/PlaylistController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using _Project.Scripts.Data;
+using _Project.Scripts.Systems;
 using UnityEngine;
 
 public class PlaylistController : MonoBehaviour
@@ -23,16 +24,28 @@ public class PlaylistController : MonoBehaviour
     public void Reset()
     {
         if (coroutine != null)
-        { StopCoroutine(coroutine); }
+        {
+            StopCoroutine(coroutine);
+            if (MusicSequence.Instance.IsPlaying)
+            {
+                MusicSequence.Instance.Reset();
+            }
+        }
     }
 
     private IEnumerator IteratePlaylist()
     {
         foreach (PlaylistItem item in currentPlaylist.playlistItems)
         {
-            Debug.Log($"Starting {item.duration}");
+            if (item.track.name.Trim().ToLower() == "break")
+            {
+                yield return new WaitForSeconds(item.duration);
+                continue;
+            }
+            EventManager.InvokeMusicSettingChangeEvent(item.track);
+            MusicSequence.Instance.Play();
             yield return new WaitForSeconds(item.duration);
-            Debug.Log($"Ending {item.duration}");
+            MusicSequence.Instance.Reset();
         }
     }
 
